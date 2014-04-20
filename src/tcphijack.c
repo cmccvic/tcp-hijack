180dd12426c66569196d8832dd4f5e0eba3f8fc1
Michael Salerno <michael.j.salerno@stonybrook.edu>
2014-04-19 23:57:36 -0400

    added stuff to the header

--- src/tcphijack.c
+++ src/tcphijack.c
@@ -1,23 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <string.h> //memset()
-#include <unistd.h> //sleep()
-
-//Socket stuff
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
-//IP header (struct iphdr) definition
-#include <linux/ip.h>
-//TCP header (struct tcphdr) definition
-#include <linux/tcp.h>
-
-//Perhaps these headers are more general
-//#include <netinet/tcp.h>
-//#include <netinet/ip.h>
-
+#include "tcphijack.h"
 //Data to be sent (appended at the end of the TCP header)
 //#define DATA "datastring"
 #define DATA ""
