21eff43dc1a751f8a7438ad001018006aa3ee4db
Adam Przybyszewski <spdc88@gmail.com>
2014-05-02 00:13:47 -0400

    Added ifdef guards for packet-sniff.c

--- include/packet-sniff.h
+++ include/packet-sniff.h
@@ -1,3 +1,8 @@
+#ifndef PACKET_SNIFF_H
+#define PACKET_SNIFF_H
+
+
+/* Include dependencies. */
 #include <arpa/inet.h>
 #include <ctype.h>
 #include <netinet/ip_icmp.h>
@@ -14,7 +19,7 @@
 #define TCP_HEADER_SIZE 20
 
 
-/* DNS Header bit flags as defined in RFC1035 */
+/* DNS Header bit flags as defined in RFC1035. */
 #define DNS_HEADER_QR_QUERY					0x0000
 #define DNS_HEADER_QR_RESPONSE 				0x8000
 #define DNS_HEADER_OPCODE_QUERY				0x0000
@@ -59,3 +64,6 @@ void processPacket(u_char *arg, const struct pcap_pkthdr *pkthdr, const u_char *
  * pcap library to automatically select a device on the computer.
  */
 void tcpDisrupt(char *clientIP, char *serverIP, char *networkInterface);
+
+
+#endif
