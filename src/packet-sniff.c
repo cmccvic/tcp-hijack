ca0daae80eb816a9fc64ccdab1ab5d362313adc4
r0t0xd <spdc88@gmail.com>
2014-05-06 15:20:47 -0400

    changed location of where data is stored in packet-sniff. this code is a mess and needs to be rewritten, although it is functioning as intended

--- src/packet-sniff.c
+++ src/packet-sniff.c
@@ -1,112 +1,111 @@
 #include "tcp-disrupt.h"
 
-int tcpDisrupt(char *clientIP, char *serverIP, char *serverPort, char *networkInterface){
-    char                errbuf[PCAP_ERRBUF_SIZE];
-    char                packetFilterString[256];
-    char                *device;
-    int                 datalinkType;
-    int                 maxBytesToCapture = 65535;
-    pcap_t              *packetDescriptor;
-    struct bpf_program  packetFilter;
-    uint32_t            srcIP;
-    uint32_t            netmask;
+int sniffNetwork(sniffArgs *sniffArgs){
+    char                errbuf[PCAP_ERRBUF_SIZE];   // The string buffer to store pcap errors in.
+    char                packetFilterString[256];    // The string representation of the filter to apply to pcap.
+    struct bpf_program  packetFilter;               // The pcap version of the filter to apply.
+    int                 maxBytesToCapture = 65535;  // The maximum number of bytes to capture in a packet.
+    pcap_t              *packetDescriptor;          // The socket descriptor for our nic.
+    char                *nicName = NULL;            // The name of the NIC we are using.
+    int                 nicType = -1;               // The type of NIC we are using.
+    bpf_u_int32         nicNetMask;                 // The netmask associated with the NIC.
+    bpf_u_int32         nicAddress;                 // The IP address associated with the NIC.
 
-    spdcxSniffArgs *sniffArgs;
-    if( !(sniffArgs = malloc(sizeof(spdcxSniffArgs))) ){
+    processPacketArgs *processPacketArgs;
+    if( !(processPacketArgs = malloc(sizeof(struct processPacketArgs))) ){
         fprintf(stderr, "[FAIL] Unable to obtain memory.\n");
         return 1;
-    }
-
-    /* Initialize data: */
-    memset(sniffArgs, 0, sizeof(spdcxSniffArgs));
+    } 
+    memset(processPacketArgs, 0, sizeof(struct processPacketArgs));
     memset(errbuf, 0, PCAP_ERRBUF_SIZE);
     memset(packetFilterString, 0, 256);
 
-    clientIP = clientIP ? clientIP : DEFAULT_CLIENT_IP;
-    serverIP = serverIP ? serverIP : DEFAULT_SERVER_IP;
-    serverPort = serverPort ? serverPort : DEFAULT_SERVER_PORT;
-    sniffArgs->clientIP = clientIP;
-    sniffArgs->serverIP = serverIP;
-    sniffArgs->serverPort = atoi(serverPort);
+    /* Determine the IP addresseses and ports to monitor and create a filter*/
+    char *clientIP = sniffArgs->clientIP ? sniffArgs->clientIP : DEFAULT_CLIENT_IP;
+    char *serverIP = sniffArgs->serverIP ? sniffArgs->serverIP : DEFAULT_SERVER_IP;
+    int     serverPort = sniffArgs->serverPort ? sniffArgs->serverPort : DEFAULT_SERVER_PORT; 
+    snprintf(packetFilterString, 256, "tcp and port %d and host %s and host %s", serverPort, serverIP, clientIP);
 
-    snprintf(packetFilterString, 256, "tcp and port %s and host %s and host %s", serverPort, serverIP, clientIP);
+    processPacketArgs->clientIP = clientIP;
+    processPacketArgs->serverIP = serverIP;
+    processPacketArgs->serverPort = serverPort;
 
     /* Determine the name of the network interface we are going to use: */
-    if ( !networkInterface ){
-        if ( (device = pcap_lookupdev(errbuf)) <= 0 ){
+    if ( !sniffArgs->interface ){
+        if ( (nicName = pcap_lookupdev(errbuf)) <= 0 ){
             fprintf(stderr, "[FAIL] pcap_lookupdev returned: \"%s\"\n", errbuf);
-            free(sniffArgs);
+            free(processPacketArgs);
             return 1;
-        } else printf("[INFO] Found Hardware: %s\n", device);
-    } else device = networkInterface;
+        } else printf("[INFO] Found Hardware: %s\n", nicName);
+    } else nicName = sniffArgs->interface;
 
     /* Obtain a descriptor to monitor the hardware: */
-    if ( (packetDescriptor = pcap_open_live(device, maxBytesToCapture, 1, 512, errbuf)) < 0 ){
+    if ( (packetDescriptor = pcap_open_live(nicName, maxBytesToCapture, 1, 512, errbuf)) < 0 ){
         fprintf(stderr, "[FAIL] pcap_open_live returned: \"%s\"\n", errbuf);
-        free(sniffArgs);
+        free(processPacketArgs);
         return 1;
     } else printf("[INFO] Obtained Socket Descriptor.\n");
 
     /* Determine the data link type of the descriptor we obtained: */
-    if ((datalinkType = pcap_datalink(packetDescriptor)) < 0 ){
+    if ((nicType = pcap_datalink(packetDescriptor)) < 0 ){
         fprintf(stderr, "[FAIL] pcap_datalink returned: \"%s\"\n", pcap_geterr(packetDescriptor));
-        free(sniffArgs);
+        free(processPacketArgs);
         return 2;
-    } else printf("[INFO] Obtained Data Link Type: %d\n", datalinkType);
+    } else printf("[INFO] Obtained Data Link Type: %d\n", nicType);
  
     /* Determine the header length of the data link layer: */
-    switch (datalinkType) {
+    switch (nicType) {
         case DLT_NULL:
-            sniffArgs->dataLinkOffset = 4;
+            processPacketArgs->dataLinkOffset = 4;
             printf("Listening on an NULL connection. Data Link Offset: 14\n");
             break;
 
         case DLT_EN10MB:
-            sniffArgs->dataLinkOffset = 14;
+            processPacketArgs->dataLinkOffset = 14;
             printf("Listening on an Ethernet connection. Data Link Offset: 14\n");
             break;
      
         case DLT_SLIP:
         case DLT_PPP:
-            sniffArgs->dataLinkOffset = 24;
+            processPacketArgs->dataLinkOffset = 24;
             printf("Listening on an SLIP/PPP connection. Data Link Offset: 14\n");
             break;
      
         case DLT_IEEE802_11:
-            sniffArgs->dataLinkOffset = 22;
+            processPacketArgs->dataLinkOffset = 22;
             printf("Listening on an Wireless connection. Data Link Offset: 22\n");
             break;
         
         default:
-            printf("[ERROR]: Unsupported datalink type: %d\n", datalinkType);
-            free(sniffArgs);
+            printf("[ERROR]: Unsupported datalink type: %d\n", nicType);
+            free(processPacketArgs);
             return 2;
     }
 
-    // Get network device source IP address and netmask.
-    if (pcap_lookupnet(device, &srcIP, &netmask, errbuf) < 0){
+    // Get network nic source IP address and nicNetMask.
+    if (pcap_lookupnet(nicName, &nicAddress, &nicNetMask, errbuf) < 0){
         printf("[FAIL] pcap_lookupnet returned: \"%s\"\n", errbuf);
-        free(sniffArgs);
+        free(processPacketArgs);
         return 1;
-    } else printf("[INFO] Source IP/Netmask: 0x%x/0x%x\n", srcIP, netmask);
+    } else printf("[INFO] nic IP/Netmask: 0x%x/0x%x\n", nicAddress, nicNetMask);
 
     // Convert the packet filter epxression into a packet  filter binary.
-    if (pcap_compile(packetDescriptor, &packetFilter, packetFilterString, 0, netmask)){
+    if (pcap_compile(packetDescriptor, &packetFilter, packetFilterString, 0, nicNetMask)){
         printf("[FAIL] pcap_compile returned: \"%s\"\n", pcap_geterr(packetDescriptor));
-        free(sniffArgs);
+        free(processPacketArgs);
         return 1;
     } 
 
     // Assign the packet filter to the given libpcap socket.
     if (pcap_setfilter(packetDescriptor, &packetFilter) < 0) {
         printf("[FAIL] pcap_setfilter returned: \"%s\"\n", pcap_geterr(packetDescriptor));
-        free(sniffArgs);
+        free(processPacketArgs);
         return 1;
     } else printf("[INFO] Packet Filter: %s\n", packetFilterString);
 
     /* Start the loop: */
-    pcap_loop(packetDescriptor, -1, processPacket, (u_char *)sniffArgs);
-    free(sniffArgs);
+    pcap_loop(packetDescriptor, -1, processPacket, (u_char *)processPacketArgs);
+    free(processPacketArgs);
     return 0;
 }
 
@@ -117,11 +116,17 @@ void processPacket(u_char *arg, const struct pcap_pkthdr *pktHeader, const u_cha
     int             dataLength;
     struct ip       *ipHeader;
     struct tcphdr   *tcpHeader;
-    spdcxSniffArgs  *sniffArgs = (spdcxSniffArgs *)arg;;
-    sniffArgs->packetCount++;
+
+    processPacketArgs  *processPacketArgs = (struct processPacketArgs *)arg;;
 
     /* Navigate past the Data Link layer to the Network layer: */
-    packet += sniffArgs->dataLinkOffset;
+    packet += processPacketArgs->dataLinkOffset;
+ 
+    /* Increment the number of packets we have processed: */
+    processPacketArgs->packetCount++;
+
+ 
+
     ipHeader = (struct ip*)packet;
     dataLength = ntohs(ipHeader->ip_len) - (4 * ipHeader->ip_hl);
     strcpy(srcIP, inet_ntoa(ipHeader->ip_src));
@@ -132,17 +137,12 @@ void processPacket(u_char *arg, const struct pcap_pkthdr *pktHeader, const u_cha
     switch (ipHeader->ip_p) {
         case IPPROTO_TCP:
             tcpHeader = (struct tcphdr*)packet;
-            dataLength = dataLength - TCP_HEADER_SIZE;
-            packet += TCP_HEADER_SIZE;
+            dataLength = tcpHeader->doff;
             break;
      
         default:
             break;
     }
-
-    /* Skip the options in the tcp header: */
-    packet += 12;
-    dataLength -= 12;
 
     /* Navigate past the Transport layer to the payload: */
     if(dataLength > 0){
@@ -151,19 +151,16 @@ void processPacket(u_char *arg, const struct pcap_pkthdr *pktHeader, const u_cha
         uint16_t sourcePort = ntohs(tcpHeader->source);
         uint16_t destPort = ntohs(tcpHeader->dest);
 
-        if ( !(strcmp(sniffArgs->clientIP, dstIP)) ) {
+        if ( !(strcmp(processPacketArgs->clientIP, dstIP)) ) {
             printf("[processPocket]: Matching client IP found: %s\n", dstIP);
-            if ( !(strcmp(sniffArgs->serverIP, srcIP)) ) {
+            if ( !(strcmp(processPacketArgs->serverIP, srcIP)) ) {
                 printf("[processPocket]: Matching server IP found: %s\n", srcIP);
-                if ( sniffArgs->serverPort == sourcePort ){
+                if ( processPacketArgs->serverPort == sourcePort ){
                     printf("[processPocket]: Matching server port found: %d\n", destPort);
                     printf("[processPocket]: sequence Number: %u\n", (uint32_t)seqNum);
                     printf("[processPocket]: ack Number: %u\n", (uint32_t)ackNum);                    
                     printf("[processPocket]: Disrupting.....\n");
-                    if(sniffArgs->packetCount < 100)
-                        disrupt_session(srcIP, sourcePort, dstIP, destPort, seqNum, ackNum, 0, 0);
-                    else
-                        disrupt_session(srcIP, sourcePort, dstIP, destPort, seqNum, ackNum, 0, 1);
+                    disrupt_session((void *)ipHeader);
                 }
             }
         }
