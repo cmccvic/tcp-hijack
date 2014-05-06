752df610cd0fb69d74022b4332ba2db2e8340dd5
r0t0xd <spdc88@gmail.com>
2014-05-06 15:14:55 -0400

    changed disrupt_session to only accept the packet instead of a million arguments

--- include/tcp-disrupt.h
+++ include/tcp-disrupt.h
@@ -37,9 +37,12 @@
 #define PSH_OFF 0
 #define RESET_OFF 0
 
+
 /* Displays the usage string for this program */
 void display_usage(char *name);
 
-void disrupt_session(char *sourceIP, uint16_t sourcePort, char *destinationIP, uint16_t destinationPort, uint32_t sequenceNumber, uint32_t ackNumber, int timestamp, int finalRound);
+
+void disrupt_session(void *packet);
+
 
 #endif
